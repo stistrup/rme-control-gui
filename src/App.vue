4738be15290f5231d66d4edf2592522d438dae4b
Stefan Stistrup <sstistrup@gmail.com>
2025-08-08 19:08:17 +0200

    remove unused mqtt, and update vulnerable package

--- src/App.vue
+++ src/App.vue
@@ -3,52 +3,15 @@ import { inject, onBeforeUnmount, onMounted } from "vue";
 import HomeView from "./views/HomeView.vue";
 import { RmeService } from "./services/RmeService.ts";
 import { useRmeStore } from "./stores/rmeStore.ts";
-import { MqttService } from "./services/MqttService.ts";
-import { percentageToDb } from "./utils/alsaValConversion.ts";
-import { OutputType } from "./types/config.types.ts";
-import { BrokerConfig } from "./types/mqtt.types.ts";
-
 const rmeService = inject<RmeService>("RmeService");
-const mqttService = inject<MqttService>("MqttService");
 const rmeStore = useRmeStore();
 
-if (!rmeService || !mqttService) {
-  throw new Error("Could not import plugins");
+if (!rmeService) {
+  throw new Error("Could not import plugin");
 }
 
 const initApp = async () => {
   await rmeService.init();
-
-  let mqttConfig: BrokerConfig;
-
-  try {
-      // This is not included in release or in source code
-      // This is an extra feature for myself to control headphones and phantom power
-      // from a custom physical device
-      const mqttConfResponse = await fetch("mqttConf.json")
-      mqttConfig = await mqttConfResponse.json() as BrokerConfig
-
-      if (mqttConfig) {
-        await mqttService.init(mqttConfig.broker)
-      }
-  } catch (e) {
-      console.warn("Not MQTT config. MQTT feature disabled")
-  }
-
-  const hpConf = rmeStore.soundCardConfig.outputs.find(out => out.type === OutputType.HEADPHONES)
-
-  mqttService.on("volume", (volume) => {
-    if (hpConf) {
-      const dbValue = percentageToDb(volume)
-      rmeService.setAlsaVolumeStereo(hpConf.controlNameLeft, hpConf.controlNameRight, dbValue)
-      rmeStore.setMainVolume(hpConf.controlNameLeft, dbValue, dbValue)
-    }
-  })
-
-  mqttService.on("phantom", (phantomState) => {
-    // index 0 === mic 1
-    rmeService.setPhantomPower(0, phantomState)
-  })
 };
 
 onMounted(async () => {
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import App from "./App.vue";
 import "./assets/base.css";
 import { createPinia } from "pinia";
 import RmePlugin from "./services/RmeService";
-import MqttPlugin from "./services/MqttService";
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 app.use(RmePlugin);
-app.use(MqttPlugin);
 app.mount("#app");
